53c4eef48f5848c8659b9cf2d9c0b1659503cf95
SonicMaster455 <jordy@pintu.co.id>
2018-12-10 05:52:09 +0700

    split order

--- app/Http/Controllers/CoinController.php
+++ app/Http/Controllers/CoinController.php
@@ -17,23 +17,19 @@ class CoinController extends Controller
         $this->apiIndodax = new ApiIndodax();
     }
 
-    public function transaction(Request $request) {
+    public function orderBuy(Request $request) {
         $validator = Validator::make(
             [
                 'method' => $request->method,
                 'pair' => $request->pair,
                 'type' => $request->type,
                 'price' => $request->price,
-                'idr' => $request->idr,
-                'btc' => $request->btc,
             ],
             [
                 'method' => 'required',
                 'pair' => 'required',
                 'type' => 'required',
                 'price' => 'required',
-                'idr' => 'required',
-                'btc' => 'required',
             ]
         );
 
@@ -47,6 +43,167 @@ class CoinController extends Controller
                 'price' => $request->price,
                 'idr' => $request->idr,
                 'btc' => $request->btc,
+            ];
+    
+            $result = $this->apiIndodax->transaction($data);
+            $resultJson = json_decode(json_encode($result), true);
+    
+            return response($resultJson, 200);
+        } else {
+            $messages = json_decode($validator->messages());
+
+            $mess = '';
+            foreach ($messages as $key => $val) {
+                $mess = $val[0];
+                break;
+            }
+
+            return response(400, $mess);
+        }
+    }
+
+    public function orderSell(Request $request) {
+        $validator = Validator::make(
+            [
+                'method' => $request->method,
+                'pair' => $request->pair,
+                'type' => $request->type,
+                'price' => $request->price,
+                'idr' => $request->idr,
+                'symbol' => $request->symbol,
+                'amount' => $request->amount,
+            ],
+            [
+                'method' => 'required',
+                'pair' => 'required',
+                'type' => 'required',
+                'price' => 'required',
+            ]
+        );
+
+        if (!$validator->fails()) {
+            $data = [
+                'method' => $request->method,
+                'nonce' => time(),
+                'pair' => $request->pair,
+                'type' => $request->type,
+                'price' => $request->price,
+                'idr' => $request->idr,
+                $request->symbol => $request->amount,
+            ];
+    
+            $result = $this->apiIndodax->transaction($data);
+            $resultJson = json_decode(json_encode($result), true);
+    
+            return response($resultJson, 200);
+        } else {
+            $messages = json_decode($validator->messages());
+
+            $mess = '';
+            foreach ($messages as $key => $val) {
+                $mess = $val[0];
+                break;
+            }
+
+            return response(400, $mess);
+        }
+    }
+
+    public function cancelOrder(Request $request) {
+        $validator = Validator::make(
+            [
+                'pair' => $request->pair,
+                'order_id' => $request->order_id,
+                'type' => $request->type,
+            ],
+            [
+                'pair' => 'required',
+                'order_id' => 'required',
+                'type' => 'required',
+            ]
+        );
+
+        if (!$validator->fails()) {
+
+            $data = [
+                'method' => $request->method,
+                'nonce' => time(),
+                'pair' => $request->pair,
+                'order_id' => $request->order_id,
+                'type' => $request->type,
+            ];
+    
+            $result = $this->apiIndodax->transaction($data);
+            $resultJson = json_decode(json_encode($result), true);
+    
+            return response($resultJson, 200);
+        } else {
+            $messages = json_decode($validator->messages());
+
+            $mess = '';
+            foreach ($messages as $key => $val) {
+                $mess = $val[0];
+                break;
+            }
+
+            return response(400, $mess);
+        }
+    }
+
+    public function openOrder(Request $request) {
+        $validator = Validator::make(
+            [
+                'pair' => $request->pair,
+            ],
+            [
+                'pair' => 'required',
+            ]
+        );
+
+        if (!$validator->fails()) {
+
+            $data = [
+                'method' => $request->method,
+                'nonce' => time(),
+                'pair' => $request->pair,
+            ];
+    
+            $result = $this->apiIndodax->transaction($data);
+            $resultJson = json_decode(json_encode($result), true);
+    
+            return response($resultJson, 200);
+        } else {
+            $messages = json_decode($validator->messages());
+
+            $mess = '';
+            foreach ($messages as $key => $val) {
+                $mess = $val[0];
+                break;
+            }
+
+            return response(400, $mess);
+        }
+    }
+
+    public function getOrder(Request $request) {
+        $validator = Validator::make(
+            [
+                'pair' => $request->pair,
+                'order_id' => $request->order_id,
+            ],
+            [
+                'pair' => 'required',
+                'order_id' => 'required',
+            ]
+        );
+
+        if (!$validator->fails()) {
+
+            $data = [
+                'method' => $request->method,
+                'nonce' => time(),
+                'pair' => $request->pair,
+                'order_id' => $request->order_id,
             ];
     
             $result = $this->apiIndodax->transaction($data);
--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-$router->group(['prefix' => 'coin'], function () use ($router) {
-    $router->post('transaction', 'CoinController@transaction');
+$router->group(['prefix' => 'exchange'], function () use ($router) {
+    $router->post('buy', 'CoinController@orderBuy');
+    $router->post('sell', 'CoinController@orderSell');
+    $router->post('cancel', 'CoinController@cancelOrder');
+    $router->post('open', 'CoinController@openOrder');
+    $router->post('detail', 'CoinController@getOrder');
 });
